bb98318258696f94a3bb4e6338056075a43601b2
ilyasemikin <iasemikin@gmail.com>
2024-04-13 16:35:13 +0300

    Disable transitive dependencies

--- _Src/Freem/Freem.Entities/Extensions/RunningRecordExtensions.cs
+++ _Src/Freem/Freem.Entities/Extensions/RunningRecordExtensions.cs
@@ -1,10 +1,12 @@
-﻿namespace Freem.Entities.Extensions;
+﻿using Freem.DateTimePeriods;
+
+namespace Freem.Entities.Extensions;
 
 public static class RunningRecordExtensions
 {
     public static Record ToRecord(this RunningRecord running, string id, DateTimeOffset endAt)
     {
-        var period = new TimePeriod(running.StartAt, endAt);
+        var period = new DateTimePeriod(running.StartAt, endAt);
         return new Record(id, running.UserId, running.CategoryIds, running.TagIds, period);
     }
 }
